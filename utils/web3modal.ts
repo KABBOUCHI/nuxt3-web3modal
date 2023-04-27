0a8a3e32edf958204b1905648b6e0da30c908aa3
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2023-04-27 14:31:42 +0300

    Update web3modal.ts

--- utils/web3modal.ts
+++ utils/web3modal.ts
@@ -1,9 +1,26 @@
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/html'
 import { configureChains, createClient } from '@wagmi/core'
-import { arbitrum, mainnet, polygon } from '@wagmi/core/chains'
+import { Chain, arbitrum, mainnet, polygon } from '@wagmi/core/chains'
 
-const chains = [arbitrum, mainnet, polygon]
+const chains: Chain[] = [arbitrum, mainnet, polygon, {
+    id: 634,
+    name: "Avocado",
+    network: "avocado",
+    nativeCurrency: {
+        name: "Avocado",
+        symbol: "USDC",
+        decimals: 18,
+    },
+    rpcUrls: {
+        default: {
+            http: ["https://rpc.avocado.instadapp.io"]
+        },
+        public: {
+            http: ["https://rpc.avocado.instadapp.io"]
+        },
+    }
+}]
 const projectId = '42e9e3b646c9102371bd147b3e960c39'
 
 const { provider } = configureChains(chains, [w3mProvider({ projectId })])
